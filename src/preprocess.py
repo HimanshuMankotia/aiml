25d6a79fd826bd7205213cf8a666d1463238e890
HimanshuMankotia <144589453+HimanshuMankotia@users.noreply.github.com>
2026-03-15 21:40:36 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,8 +1,9 @@
 import nltk
 import re
 
-# Download required NLTK data
+# Download required NLTK resources
 nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
 
 from nltk.corpus import stopwords
@@ -11,13 +12,13 @@ from nltk.tokenize import word_tokenize
 stop_words = set(stopwords.words('english'))
 
 def preprocess_text(text):
-    
+
     text = text.lower()
-    
+
     text = re.sub(r'[^a-zA-Z\s]', '', text)
-    
+
     tokens = word_tokenize(text)
-    
+
     filtered_tokens = [word for word in tokens if word not in stop_words]
-    
+
     return " ".join(filtered_tokens)
