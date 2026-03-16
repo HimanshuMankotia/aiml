52a429a5cc244da6a737dcd2d05152d9ac4ebcc0
HimanshuMankotia <144589453+HimanshuMankotia@users.noreply.github.com>
2026-03-17 01:32:21 +0530

    Update pdf_parser.py

--- src/pdf_parser.py
+++ src/pdf_parser.py
@@ -1,10 +1,14 @@
 import pdfplumber
 
 def extract_text_from_pdf(file):
+
     text = ""
-    
+
     with pdfplumber.open(file) as pdf:
         for page in pdf.pages:
-            text += page.extract_text() or ""
-    
+            page_text = page.extract_text()
+
+            if page_text:
+                text += page_text
+
     return text
